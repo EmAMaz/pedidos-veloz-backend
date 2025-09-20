8cb73107a0d2665aaa19e91c89105efebca81d31
emamaz <santiagomaza33@gmail.com>
2025-09-19 22:50:18 -0300

    implementación del middleware Auth

--- src/routes/CategoriasRoutes.ts
+++ src/routes/CategoriasRoutes.ts
@@ -1,13 +1,14 @@
 import express from "express";
 import categoriasController from "../controllers/categoriasController";
+import { authenticateJWT } from "../middlewares/authenticateJWT";
 
 const router = express.Router();
 
 router.get("/", categoriasController.getCategory);
-router.get("/:id", categoriasController.getCategoryById);
+router.get("/:id", authenticateJWT, categoriasController.getCategoryById);
 
 
-router.post("/", categoriasController.createCategory);
-router.put("/:id", categoriasController.updateCategory)
+router.post("/", authenticateJWT, categoriasController.createCategory);
+router.put("/:id", authenticateJWT, categoriasController.updateCategory)
 
 export default router;
--- src/routes/ProductoRoutes.ts
+++ src/routes/ProductoRoutes.ts
@@ -1,14 +1,15 @@
 import express from "express";
 import productoController from "../controllers/productoController";
 import validator from "../validators/validator";
+import { authenticateJWT } from "../middlewares/authenticateJWT";
 
 const router = express.Router();
 
 router.get("/", productoController.consultarProductos);
 
-router.post("/", validator.emptyFields(["name", "price", "category"]) ,productoController.crearProducto);
-router.put("/:id", productoController.actualizarProducto)
+router.post("/", authenticateJWT, validator.emptyFields(["name", "price", "category"]), productoController.crearProducto);
+router.put("/:id", authenticateJWT, productoController.actualizarProducto)
 router.get("/filter", productoController.filterProductsByType)
-router.delete("/:id", productoController.deleteProduct)
+router.delete("/:id", authenticateJWT, productoController.deleteProduct)
 
 export default router;
