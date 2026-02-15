18be31ce9cb1c2de08d0fba0bf85591fe334837b
emamaz <santiagomaza33@gmail.com>
2026-02-15 16:45:20 -0300

    implementacion de middleware

--- src/routes/UsuarioRoutes.ts
+++ src/routes/UsuarioRoutes.ts
@@ -1,11 +1,12 @@
 import express from "express";
 import usuariosController from "../controllers/usuarioController";
+import { authenticateJWT } from "../middlewares/authenticateJWT";
 
 const router = express.Router();
 
 router.post("/registro", usuariosController.createUser)
 router.post("/login", usuariosController.loginUser)
 router.delete("/:id", usuariosController.deleteUser)
-router.post("/token", usuariosController.tokenExpired)
+router.get("/token", authenticateJWT, usuariosController.tokenExpired)
 
 export default router;
