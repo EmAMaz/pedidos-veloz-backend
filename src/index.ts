3a0116efcbc562f4d26c2e8c174dd5f18f02cb8f
emamaz <santiagomaza33@gmail.com>
2026-02-15 16:46:04 -0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import { AppDataSource } from "./db/connect";
 import "reflect-metadata";
 import "dotenv/config";
 import { configDotenv } from "dotenv";
+import { runSeeders } from "typeorm-extension";
 
 configDotenv();
 const port: number = process.env.PORT ? Number(process.env.PORT) : 3001;
@@ -10,12 +11,19 @@ const port: number = process.env.PORT ? Number(process.env.PORT) : 3001;
 async function main() {
   try {
     await AppDataSource.initialize();
+    console.log("Base de datos conectada con éxito");
+
+    if (process.env.RUN_SEEDS === "true") {
+      await runSeeders(AppDataSource);
+    }
+
     app.listen(port, () => {
       console.log("Server activo en el puerto", port);
     });
+
   } catch (err) {
     if (err instanceof Error) {
-      console.log(err.message);
+      console.error("Error durante el inicio:", err.message);
     }
   }
 }
