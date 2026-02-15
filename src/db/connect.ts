abd7eaa6da3cd0ff879b7352c7d4f0618f03e8a1
emamaz <santiagomaza33@gmail.com>
2026-02-15 16:44:51 -0300

    Update connect.ts

--- src/db/connect.ts
+++ src/db/connect.ts
@@ -1,21 +1,25 @@
-import { DataSource } from "typeorm";
+import { DataSource, DataSourceOptions } from "typeorm";
 import { Producto } from "../models/ProductoModel";
 import { Categoria } from "../models/CategoriaModel";
 import { Usuario } from "../models/UsuarioModel";
 import { configDotenv } from "dotenv";
+import { SeederOptions } from "typeorm-extension";
+import ProductoSeeder from "../seeds/producto.seeder";
+import CategoriaSeeder from "../seeds/categoria.seeder";
+import UserSeeder from "../seeds/user.seeder";
 
 configDotenv();
 
-export const AppDataSource = new DataSource({
-  type: "mysql",
-  host: process.env.DB_HOST,
+const options: DataSourceOptions & SeederOptions = {
+  type: "postgres",
+  url: process.env.DATABASE_URL_DEV,
   port: process.env.DB_PORT ? Number(process.env.DB_PORT) : 3306,
-  username: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
   synchronize: false,
   logging: true,
   entities: [Producto, Categoria, Usuario],
   subscribers: [],
-  migrations: [],
-});
+  migrations: ["./src/migrations/*.ts"],
+  seeds: [],
+}
+
+export const AppDataSource = new DataSource(options);
